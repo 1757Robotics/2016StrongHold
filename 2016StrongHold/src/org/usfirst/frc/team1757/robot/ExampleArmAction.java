fbaa8cbd7b8d9feafdfefadb2edf6157492c936e
acabey <acabey@users.noreply.github.com>
2016-02-17 21:11:16 -0500

    Delete ExampleArmAction.java
    Attempting to make a purely example project

--- 2016StrongHold/src/org/usfirst/frc/team1757/robot/ExampleArmAction.java
+++
@@ -1,8 +0,0 @@
-package org.usfirst.frc.team1757.robot;
-import edu.wpi.first.wpilibj.command.*;
-
-public class ExampleArmAction extends CommandGroup {
-	public String Test(){
-		return "fuck";
-	}
-}
